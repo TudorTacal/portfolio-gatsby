91a54c530abf3108a549fc5a07093ea847091b0b
Tudor Tacal <t.tacal@elsevier.com>
2018-05-23 23:31:57 +0100

    Remove LN projects

--- src/components/projects.js
+++ src/components/projects.js
@@ -45,35 +45,11 @@ const Projects = () => ({
             title="UdaciCards"
             abstract="The UdaciCards project is a mobile application, designed for iOS, that allows users to study collections of flashcards."
           />
-           <Project
-            logo={""}
-            url="/redactor"
-            title="REDACTOR"
-            abstract="A redaction tool built for law firms leveraging machine learning and AI. Front end app React & Redux, .NETCORE 2.0 backend, JAVA api, deployed on AWS."
-          />
-          <Project
-            logo={""}
-            url="/processflows"
-            title="ProcessFlows"
-            abstract="A visualisation tool designed for junior lawyers to navigate through their complex proccesses."
-          />
-          <Project
-            logo={""}
-            url="/curtauk"
-            title="CurtaUK"
-            abstract="A completion statement complex calculator designed for lawyers."
-          />
           <Project
             logo={""}
             url="/cabanabelvedere"
             title="CabanaBelvedere"
             abstract="A presentation website for a touristic villa, set in a spectacular natural place that is also my family's business."
-          />
-           <Project
-            logo={""}
-            url="/workflows"
-            title="Workflows"
-            abstract="Complex visualisation tools like editable checklist, related links container & navigational flowcharts meant to improve a lawyers workflow."
           />
         </Column>
       </Row>
